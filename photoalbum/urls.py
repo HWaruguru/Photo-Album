aa2a4baa8d1a5b6eb5d2ff42070efc9e13dbe186
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-13 23:03:35 +0300

    add a condition for urlpatterns

--- photoalbum/urls.py
+++ photoalbum/urls.py
@@ -1,7 +1,13 @@
 
 from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
+    url(r'^admin/', admin.site.urls),
     url('^$',views.index, name = 'index'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
