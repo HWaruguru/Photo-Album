bd6f7f9a8c625c07bbfa2841fdcc3f026adec45a
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-14 00:24:24 +0300

    Add a search view function

--- photoalbum/views.py
+++ photoalbum/views.py
@@ -4,3 +4,17 @@ from .models import Image
 def index(request):
     images = Image.objects.all()
     return render(request, "index.html", {"images": images})
+
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_image(search_term)
+        message = f"{search_term}"
+        if len(searched_images) > 0:
+            return render(request, 'search.html', {"message": message, "images": searched_images})
+        else:
+            return render(request, 'search.html', {"message": message, "term_included": True})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message, "term_included": False})
