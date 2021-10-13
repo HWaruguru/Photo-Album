b50a104ad085fd326af723b71303bf0368f604a0
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-13 23:04:41 +0300

    add images dictionary

--- photoalbum/views.py
+++ photoalbum/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from .models import Image
 
 def index(request):
-    return render(request, "index.html")
+    images = Image.objects.all()
+    return render(request, "index.html", {"images": images})
