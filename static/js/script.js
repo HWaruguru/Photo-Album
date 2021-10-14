1640ca151f1a84439f033d86e5e1d25dc5f148c8
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-14 21:48:06 +0300

    Add a location.html and scripts.js

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#ewefmwefmp").click(function(){
+    //alert()
+      var copyText = document.getElementById("myInput");
+      copyText.select();
+      copyText.setSelectionRange(0, 99999)
+      document.execCommand("copy");
+      alert("Copied the text: " + copyText.value);
+    
+    });
+});
